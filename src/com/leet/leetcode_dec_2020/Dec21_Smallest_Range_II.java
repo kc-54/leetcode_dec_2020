2e0652529de8a2dc0435a7c4a7be78bf7e003c41
stupidDummy <kelvinchandra54@yahoo.com>
2020-12-22 16:00:16 +0800

    leetcode 21/12/20 Smallest Range II

--- src/com/leet/leetcode_dec_2020/Dec21_Smallest_Range_II.java
+++ src/com/leet/leetcode_dec_2020/Dec21_Smallest_Range_II.java
@@ -12,13 +12,11 @@ public class Dec21_Smallest_Range_II {
         int mn;
 
 
-        System.out.println(res);
 
         for(int i = 0; i+1 < A.length; i++){
             mx = Math.max(mx, A[i] + 2*K);
             mn = Math.min(A[0]+2*K, A[i+1]);
             res = Math.min(res, mx-mn);
-            System.out.println(res);
         }
 
 
